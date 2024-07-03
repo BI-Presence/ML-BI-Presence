5dbc8ec5771f03327a993d92998df6706cd2a07e
AbednegoChristianyoel <abednegoefra31@gmail.com>
2024-07-03 16:08:03 +0700

    Feat: Add API Documentation with Swagger

--- model_deploy/model_api/views.py
+++ model_deploy/model_api/views.py
@@ -1,13 +1,63 @@
 from django.shortcuts import render
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response 
 from rest_framework.views import APIView
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 from model_api.services.prediction import Prediction
 
-# Create your views here.
 class PredFacenetView(APIView): 
-    def post(self,request):
-        pred_obj=Prediction()
-        response_dict=pred_obj.predict(request)
-        response=response_dict['response']
-        status_value=response_dict['status']
-        return Response(response,status_value)
+    parser_classes = (MultiPartParser, FormParser)
+
+    @swagger_auto_schema(
+        # operation_description="Upload an image file to get a prediction.",
+        manual_parameters=[
+            openapi.Parameter(
+                name='media', 
+                in_=openapi.IN_FORM, 
+                type=openapi.TYPE_FILE, 
+                description='Image file',
+                required=True
+            )
+        ],
+        responses={
+            200: openapi.Response(
+                'Success', 
+                openapi.Schema(
+                    type=openapi.TYPE_OBJECT,
+                    properties={
+                        'error': openapi.Schema(type=openapi.TYPE_STRING, description='Error status'),
+                        'message': openapi.Schema(type=openapi.TYPE_STRING, description='Message'),
+                        'predictionResult': openapi.Schema(
+                            type=openapi.TYPE_OBJECT,
+                            properties={
+                                'UserID': openapi.Schema(type=openapi.TYPE_STRING, description='User ID'),
+                                'timestamp': openapi.Schema(type=openapi.TYPE_STRING, description='Timestamp'),
+                                'confidence': openapi.Schema(type=openapi.TYPE_NUMBER, format=openapi.FORMAT_FLOAT, description='Confidence level'),
+                                'imageID': openapi.Schema(type=openapi.TYPE_INTEGER, description='Image ID'),
+                            }
+                        )
+                    }
+                ),
+                examples={
+                    'application/json': {
+                        "error": "false",
+                        "message": "success",
+                        "predictionResult": {
+                            "UserID": "vicky",
+                            "timestamp": "2024-07-03T14:05:49.439126",
+                            "confidence": 0.9121062518765518,
+                            "imageID": 13
+                        }
+                    }
+                }
+            ),
+            400: 'Bad Request',
+        }
+    )
+    def post(self, request):
+        pred_obj = Prediction()
+        response_dict = pred_obj.predict(request)
+        response = response_dict['response']
+        status_value = response_dict['status']
+        return Response(response, status=status_value)
