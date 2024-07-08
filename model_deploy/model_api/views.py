a9bc781585cd47525cd04a82a45e0842708f8d97
AbednegoChristianyoel <abednegoefra31@gmail.com>
2024-07-08 16:38:48 +0700

    feat: add face detection using live camera

--- model_deploy/model_api/views.py
+++ model_deploy/model_api/views.py
@@ -5,6 +5,46 @@ from rest_framework.views import APIView
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 from model_api.services.prediction import Prediction
+import cv2
+from django.shortcuts import render
+from django.http import StreamingHttpResponse
+
+# Index
+def index(request):
+    return render(request, 'detection/index.html')
+
+# Testing with Camera
+def detect_faces_camera(request):
+    # Initialize OpenCV Cascade Classifier
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+    # Function to generate frames from camera
+    def gen_frames():
+        cap = cv2.VideoCapture(0)
+
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            # Perform face detection
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+            # Draw rectangles around detected faces
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+            # Convert frame to JPEG format for web display
+            _, jpeg = cv2.imencode('.jpg', frame)
+            frame_bytes = jpeg.tobytes()
+
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+    return StreamingHttpResponse(gen_frames(), content_type='multipart/x-mixed-replace; boundary=frame')
+
+# Testing with Camera
 
 class PredFacenetView(APIView): 
     parser_classes = (MultiPartParser, FormParser)
