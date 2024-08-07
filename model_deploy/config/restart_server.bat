2c0081ac38d9ffd0bd050cfaa71cb52cbd58d01c
AbednegoChristianyoel <abednegoefra31@gmail.com>
2024-08-07 08:00:35 +0700

    fix: fix bug restart server django

--- model_deploy/config/restart_server.bat
+++ model_deploy/config/restart_server.bat
@@ -1,7 +1,12 @@
 @echo off
 
+REM Stop the running Django server
+echo Stopping Django server...
+taskkill /IM python.exe /F
+
+REM Wait for a moment to ensure the process is terminated
+timeout /t 2
+
 REM Restart the Django server
 echo Restarting Django server...
 python manage.py runserver
-
-REM
